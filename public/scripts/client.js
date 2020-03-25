d3550955bba8add83a3c66fdc0e9433291f92fff
bmark88 <bradley.mark@hotmail.com>
2020-03-25 23:45:12 +0000

    add refresh for tweets upon new tweet & prevent JS entries

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,63 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweetData) => {
-  const { name, avatars, handle } = tweetData.user;
-  const { text } = tweetData.content;
-  const { created_at } = tweetData;
+$(() => {
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }  
 
-  const currentTweet = `
+  const createTweetElement = (tweetData) => {
+    const { name, avatars, handle } = tweetData.user;
+    const { text } = tweetData.content;
+    const { created_at } = tweetData;
+
+    const currentTweet = `
     <article class="tweet">
       <header>
           <img src="${avatars}" alt="doge" height="125" width="125">
           ${name}
           <p class="right handle">${handle}</p>
       </header>
-      <p>${text}</p>
+      <p>${escape(text)}</p>
       <footer>
         ${created_at} <span class="tweet-icons"> <span>&#9873</span><span>&#128257</span><span>&#9829</span></span>
       </footer>
     </article>
   `;
 
-  return currentTweet;
-};
+    return currentTweet;
+  };
 
-const renderTweets = function (tweets) {
-  const tweetElements = [];
+  const renderTweets = function (tweets) {
+    const tweetElements = [];
 
-  for (let tweet of tweets) {
-    tweetElements.push(createTweetElement(tweet))
+    for (let tweet of tweets) {
+      tweetElements.push(createTweetElement(tweet))
+    }
+
+    // renders the tweets with newest appearing at the top using .reverse()
+    $('#tweet-container').html(tweetElements.reverse().join(''));
   }
 
-  // renders the tweets with newest appearing at the top using .reverse()
-  $('#tweet-container').append(tweetElements.reverse().join(''));
-}
-
-// on client tweet submission, post the text data to /tweets/ route
-$('.new-tweet-form').on('submit', function (event) {
-  event.preventDefault();
-  if ($('textarea').val().length !== 0 && $('textarea').val().length <= 140) {
+  const loadTweets = () => {
     $.ajax({
-        url: '/tweets/',
-        method: 'POST',
-        data: $(this).serialize()
-      })
-      .then(function () {
-        console.log(this.data);
-      })
-  }
-});
-
-const loadTweets = () => {
-  $.ajax({
       url: '/tweets/',
       type: 'GET',
-      dataType: "json"
+      dataType: "json",
+      success: (response) => {
+        renderTweets(response);
+      }
     })
-    .then(response => {
-      renderTweets(response);
-    })
-};
+  }
 
-loadTweets();
+  loadTweets();
+
+  // on client tweet submission, post the text data to /tweets/ route
+  $('.new-tweet-form').submit(function (event) {
+    event.preventDefault();
+    if ($('#tweet-text').val().length !== 0 && $('#tweet-text').val().length <= 140) {
+      $.ajax({
+        url: '/tweets/',
+        type: 'POST',
+        data: $(this).serialize(),
+        success: (response) => {
+          loadTweets();
+        }
+      })
+    } 
+  });
+
+});
