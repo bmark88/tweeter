cf04ac55c45af881409a20a6e829b4e00efc5a15
bmark88 <bradley.mark@hotmail.com>
2020-03-25 17:37:54 +0000

    add functions to render tweets from tweet data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweetData) => {
+  const { name, avatars, handle } = tweetData.user;
+  const { text } = tweetData.content;
+  const { created_at } = tweetData;
+
+  const currentTweet = `
+    <article class="tweet">
+      <header>
+          <img src="${avatars}" alt="doge" height="125" width="125">
+          ${name}
+          <p class="right handle">${handle}</p>
+      </header>
+      <p>${text}</p>
+      <footer>
+        ${created_at} <span class="tweet-icons"> <span>&#9873</span><span>&#128257</span><span>&#9829</span></span>
+      </footer>
+    </article>
+  `;
+
+  return currentTweet;
+};
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  const tweetElements = [];
+
+  for (let tweet of tweets) {
+    tweetElements.push(createTweetElement(tweet))
+  }
+
+  $('#tweet-container').append(tweetElements.join(''));
+}
+
+renderTweets(data);
