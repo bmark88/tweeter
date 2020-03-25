8150a2e18cd03ddeb76c5d6ea82d6971296a932a
bmark88 <bradley.mark@hotmail.com>
2020-03-25 20:12:38 +0000

    add logic to prevent empty or over 140 char tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -26,33 +26,7 @@ const createTweetElement = (tweetData) => {
   return currentTweet;
 };
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-const renderTweets = function(tweets) {
+const renderTweets = function (tweets) {
   const tweetElements = [];
 
   for (let tweet of tweets) {
@@ -63,27 +37,27 @@ const renderTweets = function(tweets) {
   $('#tweet-container').append(tweetElements.reverse().join(''));
 }
 
-// renderTweets(data);
-
 // on client tweet submission, post the text data to /tweets/ route
-$('.new-tweet-form').on('submit', function(event) {
+$('.new-tweet-form').on('submit', function (event) {
   event.preventDefault();
-  $.ajax({
-    url: '/tweets/',
-    method: 'POST',
-    data: $(this).serialize()
-  })
-    .then(function() {
-      console.log(this.data);
-    })
+  if ($('textarea').val().length !== 0 && $('textarea').val().length <= 140) {
+    $.ajax({
+        url: '/tweets/',
+        method: 'POST',
+        data: $(this).serialize()
+      })
+      .then(function () {
+        console.log(this.data);
+      })
+  }
 });
 
 const loadTweets = () => {
   $.ajax({
-    url: '/tweets/',
-    type: 'GET',
-    dataType: "json"
-  })
+      url: '/tweets/',
+      type: 'GET',
+      dataType: "json"
+    })
     .then(response => {
       renderTweets(response);
     })
